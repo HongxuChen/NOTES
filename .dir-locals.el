92ca18a3a085a83baadc92c9ea3a1c7fa1776fd2
HongxuChen <leftcopy.chx@gmail.com>
2013-05-11 02:36:58 +0800

    Add elisp-cookbook.org

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (ido-ignore-files . ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "^auto/$" "_region_" "\\.html" "\\.mm" "\\.pdf"))))
